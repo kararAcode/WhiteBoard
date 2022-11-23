031ea3f90cafed3bdc6d85812b25a948f3247f91
Liiiiyuxuan <113405136+Liiiiyuxuan@users.noreply.github.com>
2022-11-23 11:59:16 -0600

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("/login");
 });
 
 app.listen(8080, () => {
