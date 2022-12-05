cb33b205d591a1b76a777ea396414a22e11a494f
kararAcode <63989704+kararAcode@users.noreply.github.com>
2022-12-05 09:12:32 -0600

    setup tailwind actually
    for some odd reason my last commit did not work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/*.{html,js,css}",
+    "./views/*.ejs"
+  ],
+  
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
     extend: {},
   },
   plugins: [],
